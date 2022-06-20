49bbc9c4ec36105514e1dd170574a73234f421cc
28974 <germainetsakoue@yahoo.com>
2022-06-20 23:30:20 +0100

    h

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+ executable file 9 lines (9 sloc) 215 Bytes
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result

415d6f0bb06e299c4131eab48ca50f53f5283b15
28974 <germainetsakoue@yahoo.com>
2022-10-01 10:50:44 +0100

    k

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+"""Make a POST request
 """
-Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as a parameter
-"""
+
 import requests
-import sys
+from sys import argv
 
 if __name__ == '__main__':
 
-    new_value = {'email': sys.argv[2]}
-    req = requests.post(sys.argv[1], new_value)
-    print(req.text)
+    url = argv[1]
+    email = argv[2]
+
+    r = requests.post(url, data={'email': email})
+    print(r.text)
